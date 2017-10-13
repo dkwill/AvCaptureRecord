718280f0c834a5ff2dab98a345c6838679dd6c6b
dkwill <info@hittingtech.com>
2017-10-13 13:18:22 -0500

    Initial commit

--- AvCaptureRecord/ViewController.h
+++ AvCaptureRecord/ViewController.h
@@ -8,8 +8,29 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+#import <Foundation/Foundation.h>
+#import <CoreMedia/CoreMedia.h>
+#import <AVFoundation/AVFoundation.h>
 
+#import <AssetsLibrary/AssetsLibrary.h>        //<<Can delete if not storing videos to the photo library.  Delete the assetslibrary framework too requires this)
+
+#define CAPTURE_FRAMES_PER_SECOND        20
+
+@interface ViewController : UIViewController
+<AVCaptureFileOutputRecordingDelegate>
+{
+    BOOL WeAreRecording;
+    
+    AVCaptureSession *CaptureSession;
+    AVCaptureMovieFileOutput *MovieFileOutput;
+    AVCaptureDeviceInput *VideoInputDevice;
+}
+
+@property (retain) AVCaptureVideoPreviewLayer *PreviewLayer;
+
+- (void) CameraSetOutputProperties;
+- (AVCaptureDevice *) CameraWithPosition:(AVCaptureDevicePosition) Position;
+- (IBAction)StartStopButtonPressed:(id)sender;
+- (IBAction)CameraToggleButtonPressed:(id)sender;
 
 @end
-
